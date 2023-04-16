2fea9d4cddb15196999501c2f17f2193fb7e801a
Pavel Jetensky <pavel.jetensky@plus4u.net>
2023-04-16 22:39:04 +0200

    Adding code duplicity

--- src/main/java/net/jetensky/cleancode/dry/FibonacciExample1.java
+++ src/main/java/net/jetensky/cleancode/dry/FibonacciExample1.java
@@ -6,16 +6,20 @@ class FibonacciExample1 {
     int n1 = 0;
     int n2 = 1;
     int n3;
-    int i;
-    System.out.print(n1 + " " + n2); //printing 0 and 1
+        System.out.print(n1 + " " + n2); //printing 0 and 1
 
-    for (i = 2; i < 10; ++i) { //loop starts from 2 because 0 and 1 are already printed
+    for (int j = 2; j < 10; ++j) { //loop starts from 2 because 0 and 1 are already printed
       n3 = n1 + n2;
       System.out.print(" " + n3);
       n1 = n2;
       n2 = n3;
     }
-
+    for (int k = 2; k < 10; ++k) { //loop starts from 2 because 0 and 1 are already printed
+      n3 = n1 + n2;
+      System.out.print(" " + n3);
+      n1 = n2;
+      n2 = n3;
+    }
 
   }
 
